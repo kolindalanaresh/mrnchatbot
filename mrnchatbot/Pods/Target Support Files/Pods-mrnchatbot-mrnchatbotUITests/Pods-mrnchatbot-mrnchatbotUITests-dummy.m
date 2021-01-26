8b94d7be8bd5b61b669e24744a2963e6ed44a24e
Prem kumar <33660516+premkolindala@users.noreply.github.com>
2021-01-26 15:13:43 +0530

    pod installation.

---
+++ mrnchatbot/Pods/Target Support Files/Pods-mrnchatbot-mrnchatbotUITests/Pods-mrnchatbot-mrnchatbotUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mrnchatbot_mrnchatbotUITests : NSObject
+@end
+@implementation PodsDummy_Pods_mrnchatbot_mrnchatbotUITests
+@end
---
+++ mrnchatbot/Pods/Target Support Files/Pods-mrnchatbot/Pods-mrnchatbot-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_mrnchatbot : NSObject
+@end
+@implementation PodsDummy_Pods_mrnchatbot
+@end
